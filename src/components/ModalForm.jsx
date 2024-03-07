056613f238b25f100a714c1c55b2d42085b08ca0
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-03-08 02:59:09 +0300

    css

--- src/components/ModalForm.jsx
+++ src/components/ModalForm.jsx
@@ -1,85 +1,66 @@
 import PropTypes from "prop-types";
 import { useRef } from "react";
 import ReactDOM from "react-dom";
+import "./ModalForm.css";
 
-const ModalForm = (props) => {
+const ModalForm = ({ isModalShow, setIsModalShow }) => {
   let modalRef = useRef();
 
-  if (!props.isModalShow) {
+  if (!isModalShow) {
     return;
   }
 
   function checkClickOutside(e) {
     if (modalRef.current && modalRef.current == e.target) {
-      props.setIsModalShow(false);
+      setIsModalShow(false);
     }
   }
 
   return ReactDOM.createPortal(
     <div
-      className="modal"
-      id="modal_1"
       onMouseDown={checkClickOutside}
       ref={modalRef}
+      className={`popup ${isModalShow ? "open" : ""}`}
     >
-      <div className="modal-content">
-        <div className="modal-header">New Input Add</div>
-        <div className="modal-body">
-          <div className="form-input">
-            <label>Name</label>
-            <input
-              type="text"
-              name="inputName"
-              id="inputName"
-              placeholder="Enter input name"
-            />
-          </div>
-
-          <div className="form-input">
-            <label>Type</label>
-            <input
-              type="text"
-              name="inputType"
-              id="inputType"
-              placeholder="Enter input type"
-            />
-          </div>
-          <div className="form-input">
-            <label>Label</label>
-            <input
-              type="text"
-              name="inputLabel"
-              id="inputLabel"
-              placeholder="Enter input label"
-            />
-          </div>
-          <div className="form-input">
-            <label>Placeholder</label>
-            <input
-              type="text"
-              name="inputPlaceHolder"
-              id="inputPlaceHolder"
-              placeholder="Enter input palceholder"
-            />
-          </div>
-        </div>
-        <div className="modal-footer">
-          <button
-            className="bg-[#365DC0] hover:bg-blue-700 
-            text-white font-bold py-2 px-4 rounded w-50 ml-10"
-            onClick={props.closeForm}
-          >
-            Close
-          </button>
-
-          <button
-            className="bg-[#365DC0] hover:bg-blue-700 
-            text-white font-bold py-2 px-4 rounded w-50 mr-10"
-            onClick={props.closeForm}
-          >
-            Add
+      <div className="popup-content">
+        <div className="popup-header">
+          <h2>Add New Input</h2>
+          <button className="close-btn" onClick={() => setIsModalShow(false)}>
+            X
           </button>
         </div>
+
+        <form>
+          <div className="form-group">
+            <label htmlFor="inputName">Name</label>
+            <input type="text" name="inputName" id="inputName" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="inputType">Type</label>
+            <input type="text" name="inputType" id="inputType" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="inputLabel">Label</label>
+            <input type="text" name="inputLabel" id="inputLabel" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="inputPlaceHolder">Placeholder</label>
+            <input type="text" name="inputPlaceHolder" id="inputPlaceHolder" />
+          </div>
+
+          <div className="button-container">
+            <button className="submit-btn">Giriş</button>
+            <button
+              className="bottom-btn"
+              onClick={() => setIsModalShow(false)}
+            >
+              Kapat
+            </button>
+          </div>
+        </form>
       </div>
     </div>,
     document.body
